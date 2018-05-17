0fb30c8c88f250ad6988d12605345de5bc8e1e83
Dan Kozlowski <Daniel.Kozlowski@va.gov>
2018-05-17 02:09:21 -0400

    Adding test to cover all requirements

--- test/main.test.js
+++ test/main.test.js
@@ -1,11 +1,51 @@
 // A nightwatch test for full e2e testing
 
 module.exports = {
-  'default e2e tests': function(browser) {
+  'Web Page Renders': function(browser) {
     browser
       .url(browser.launchUrl)
       .waitForElementVisible('#app',5000)
-      .assert.elementPresent('.form__submit-btn')
+      .end()
+  },
+
+  'Tasks are listed': function(browser) {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('#app',5000)
+      .assert.elementPresent('.todo-list')
+      .end()
+  },
+
+  'Tasks can be added': function(browser) {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('#app',5000)
+      .setValue('.form__input', 'ToDo Test').pause(500)
+      .click('.form__submit-btn')
+      .assert.elementPresent('.todo-list__item')
+      .end()
+  },
+
+  'Tasks can be removed': function(browser) {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('#app',5000)
+      .setValue('.form__input', 'ToDo Test').pause(500)
+      .click('.form__submit-btn').pause(500)
+      .click('.todo-list__item-remove');
+      browser.elements('css selector','.to-list__item',function(result){
+        browser.assert.equal(result.value.length, 0)
+      }).end()
+  },
+
+  'Tasks can be completed': function(browser) {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('#app',5000)
+      .setValue('.form__input', 'ToDo Test').pause(500)
+      .click('.form__submit-btn').pause(500)
+      .click('.todo-list__item-content')
+      .assert.elementPresent('.fa-check')
       .end()
   }
 }
